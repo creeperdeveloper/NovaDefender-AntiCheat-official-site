e495ffb643790b7f4292521f6bec520b85439d31
creeper_developers <shuukiuesugi1@gmail.com>
2025-06-23 10:20:15 +0900

    Update next.config.mjs

--- next.config.mjs
+++ next.config.mjs
@@ -1,14 +1,12 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  eslint: {
-    ignoreDuringBuilds: true,
-  },
-  typescript: {
-    ignoreBuildErrors: true,
-  },
+  output: 'export',
+  trailingSlash: true,
   images: {
-    unoptimized: true,
+    unoptimized: true
   },
+  basePath: process.env.NODE_ENV === 'production' ? '/NovaDefender-AntiCheat-official-site' : '',
+  assetPrefix: process.env.NODE_ENV === 'production' ? '/NovaDefender-AntiCheat-official-site/' : '',
 }
 
 export default nextConfig
